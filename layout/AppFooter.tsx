aff0c9ce69d6916a26c1b4ee8d9ce9a4327147b6
yahya <yahyaest1000@gmail.com>
2024-08-03 18:45:56 +0100

    Upgrade Footer

--- layout/AppFooter.tsx
+++ layout/AppFooter.tsx
@@ -3,14 +3,40 @@
 import React, { useContext } from 'react';
 import { LayoutContext } from './context/layoutcontext';
 
+
+const socialMedia = [
+    {
+        id: 1,
+        img: "/images/git.svg",
+        link: "https://github.com/yahyaest",
+    },
+    {
+        id: 2,
+        img: "/images/link.svg",
+        link: "https://www.linkedin.com/in/machat-yahya-0667b2149/",
+    },
+];
+
 const AppFooter = () => {
     const { layoutConfig } = useContext(LayoutContext);
 
     return (
-        <div className="layout-footer">
-            <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Logo" height="20" className="mr-2" />
-            by
-            <span className="font-medium ml-2">PrimeReact</span>
+        <div className="layout-footer p-3">
+            <div className="font-medium">© 2024 All In One Trade Simulator </div>
+            <div className="flex items-center" style={{position: "absolute", right: "10px"}}>
+            {socialMedia.map((info) => (
+                <a
+                key={info.id}
+                className="w-10 h-10 mx-1 p-1 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-cyan-400"
+                style={{ borderRadius: "25%"}}
+                href={info.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <img src={info.img} alt="icons" width={20} height={20} />
+                </a>
+            ))}
+            </div>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useContext, useRef, useState } from "react";
 import Link from "next/link";
-
+import Image from "next/image";
 import { StyleClass } from "primereact/styleclass";
 import { Button } from "primereact/button";
 import { Ripple } from "primereact/ripple";
@@ -419,107 +419,6 @@ const Home: Page = () => {
           </div>
         </div>
 
-        <div className="py-4 px-4 mx-0 mt-8 lg:mx-8">
-          <div className="grid justify-content-between">
-            <div className="col-12 md:col-2" style={{ marginTop: "-1.5rem" }}>
-              <Link
-                href="/"
-                className="flex flex-wrap align-items-center justify-content-center md:justify-content-start md:mb-0 mb-3 cursor-pointer"
-              >
-                <img
-                  src={`/layout/images/${
-                    layoutConfig.colorScheme === "light"
-                      ? "logo-dark"
-                      : "logo-white"
-                  }.svg`}
-                  alt="footer sections"
-                  width="50"
-                  height="50"
-                  className="mr-2"
-                />
-                <span className="font-medium text-3xl text-900">SAKAI</span>
-              </Link>
-            </div>
-
-            <div className="col-12 md:col-10 lg:col-7">
-              <div className="grid text-center md:text-left">
-                <div className="col-12 md:col-3">
-                  <h4 className="font-medium text-2xl line-height-3 mb-3 text-900">
-                    Company
-                  </h4>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    About Us
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    News
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Investor Relations
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Careers
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer text-700">
-                    Media Kit
-                  </a>
-                </div>
-
-                <div className="col-12 md:col-3 mt-4 md:mt-0">
-                  <h4 className="font-medium text-2xl line-height-3 mb-3 text-900">
-                    Resources
-                  </h4>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Get Started
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Learn
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer text-700">
-                    Case Studies
-                  </a>
-                </div>
-
-                <div className="col-12 md:col-3 mt-4 md:mt-0">
-                  <h4 className="font-medium text-2xl line-height-3 mb-3 text-900">
-                    Community
-                  </h4>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Discord
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Events
-                    <img
-                      src="/demo/images/landing/new-badge.svg"
-                      className="ml-2"
-                      alt="badge"
-                    />
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    FAQ
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer text-700">
-                    Blog
-                  </a>
-                </div>
-
-                <div className="col-12 md:col-3 mt-4 md:mt-0">
-                  <h4 className="font-medium text-2xl line-height-3 mb-3 text-900">
-                    Legal
-                  </h4>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Brand Policy
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer mb-2 text-700">
-                    Privacy Policy
-                  </a>
-                  <a className="line-height-3 text-xl block cursor-pointer text-700">
-                    Terms of Service
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
