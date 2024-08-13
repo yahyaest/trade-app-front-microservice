d28d20aa5baeb73637e185cd8fda3ebb07a43383
yahya <yahyaest1000@gmail.com>
2024-08-13 18:32:05 +0100

    Add utility code

--- utils/utils.ts
+++ utils/utils.ts
@@ -41,3 +41,73 @@ export const formatRelativeTime = (dateString: string | Date) => {
     } ago`;
   }
 };
+
+
+export const formatDateForChartFromTimestamp = (
+  dateString: string,
+  duration: string,
+) => {
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  // '2023-09-09 21:57'
+  // '2023-09-09T21:57.042Z'
+
+  const year = dateString.split('-')[0];
+  const month = months[parseInt(dateString.split('-')[1]) - 1];
+  const day = dateString.split('-')[2].split(' ')[0];
+  const hours = dateString.includes('T') ? dateString.split('T')[1].split(':')[0] : dateString.split(' ')[1].split(':')[0];
+  const minutes = dateString.includes('T') ? dateString.split('T')[1].split(':')[1].split('.')[0] : dateString.split(' ')[1].split(':')[1];
+
+  const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
+
+  if (['1h', '3h', '12h', '24h'].indexOf(duration) !== -1) {
+    return `${hours}:${minutes}`;
+  }
+
+  if (['7d'].indexOf(duration) !== -1) {
+    return `${capitalizedMonth} ${day} ${hours}:${minutes}`;
+  }
+
+  if (['30d', '3m'].indexOf(duration) !== -1) {
+    return `${capitalizedMonth} ${day}`;
+  }
+
+  if (['1y'].indexOf(duration) !== -1) {
+    return `${capitalizedMonth} `;
+  }
+
+  if (['3y'].indexOf(duration) !== -1) {
+    return `${capitalizedMonth} ${year}`;
+  }
+
+  return `${year}`;
+};
+
+
+export const calculateListAverage = (numbers : number[]) => {
+  // Check if the input is a valid array and not empty
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+      throw new Error('Input must be a non-empty array of numbers');
+  }
+
+  // Calculate the sum of all numbers
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+
+  // Calculate the average
+  const average = sum / numbers.length;
+
+  return average;
+}
